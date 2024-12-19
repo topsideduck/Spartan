9ef18a5cb31678cc11e7169bf8595a057bfa6b93
Harikeshav R <r.harikeshav@gmail.com>
2024-12-18 21:36:43 -0500

    Create AesHandler to encrypt and decrypt data

---
+++ Spartan.Utils/Cryptography/AesHandler.cs
@@ -0,0 +1,39 @@
+using System.Security.Cryptography;
+
+namespace Spartan.Utils.Cryptography;
+
+public class AesHandler
+{
+    private readonly Aes _aes;
+
+    public byte[] AesKey
+    {
+        get => _aes.Key;
+        set => _aes.Key = value;
+    }
+    
+    public byte[] AesIv
+    {
+        get => _aes.IV;
+        set => _aes.IV = value;
+    }
+
+    public AesHandler()
+    {
+        _aes = Aes.Create();
+        _aes.Mode = CipherMode.CBC;
+        _aes.Padding = PaddingMode.PKCS7;
+    }
+    
+    public byte[] Encrypt(byte[] data)
+    {
+        using var encryptor = _aes.CreateEncryptor();
+        return encryptor.TransformFinalBlock(data, 0, data.Length);
+    }
+
+    public byte[] Decrypt(byte[] data)
+    {
+        using var decryptor = _aes.CreateDecryptor();
+        return decryptor.TransformFinalBlock(data, 0, data.Length);
+    }
+}
