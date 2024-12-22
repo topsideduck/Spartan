17b28f95da62cb04512b6267f752ce59fbd96187
Harikeshav R <r.harikeshav@gmail.com>
2024-12-21 20:53:09 -0500

    Create a basic CLI for interaction with the client

---
+++ Spartan.CLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Text;
+using Spartan.Server;
+
+namespace Spartan.CLI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var socketServer = new SocketServer(IPAddress.Parse("127.0.0.1"), 12345,
+            "/Users/hkeshavr/Developer/Spartan/Spartan.Payload/bin/Release/net9.0/publish");
+
+        while (true)
+        {
+            Console.Write("> ");
+            var input = Console.ReadLine();
+            socketServer.SendData(Encoding.UTF8.GetBytes(input));
+            var response = Encoding.UTF8.GetString(socketServer.ReceiveData()).Replace("\0", string.Empty);
+            Console.WriteLine(response);
+        }
+    }
+}
