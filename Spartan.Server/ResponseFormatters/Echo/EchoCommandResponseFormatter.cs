0bb921aa514b3af6ed3ffb8af04a1084116ab40b
Harikeshav R <r.harikeshav@gmail.com>
2024-12-24 21:34:54 -0500

    Implement the echo command response formatter

--- Spartan.Server/ResponseFormatters/Echo/EchoCommandResponseFormatter.cs
+++ Spartan.Server/ResponseFormatters/Echo/EchoCommandResponseFormatter.cs
@@ -1,5 +1,13 @@
+using Spartan.Models.ResponseModels;
+using Spartan.Models.ResponseModels.Echo;
+
 namespace Spartan.Server.ResponseFormatters.Echo;
 
-public class EchoCommandResponseFormatter
+public class EchoCommandResponseFormatter : IResponseFormatter
 {
+    public string FormatResponse(ICommandResponseModel response)
+    {
+        var echoResponse = (EchoCommandResponseModel)response;
+        return echoResponse.Output;
+    }
 }
