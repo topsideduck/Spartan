6da1a673ab9ea43a0584e98794696a433c16f55d
Harikeshav R <r.harikeshav@gmail.com>
2024-12-25 10:51:19 -0500

    Fix typo

--- Spartan.Models/RequestModels/Filesystem/MkdirCommandRequestModel.cs
+++ Spartan.Models/RequestModels/Filesystem/MkdirCommandRequestModel.cs
@@ -5,6 +5,6 @@ namespace Spartan.Models.RequestModels.Filesystem;
 [MessagePackObject]
 public class MkdirCommandRequestModel : ICommandRequestModel
 {
-    [Key("DirectoryPath")] public required string directoryPath { get; set; }
+    [Key("DirectoryPath")] public required string DirectoryPath { get; set; }
     [Key("Command")] public string Command { get; set; } = "mkdir";
 }
