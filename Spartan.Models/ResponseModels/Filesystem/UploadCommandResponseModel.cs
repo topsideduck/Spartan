6bb55771e359819d09e5bb29122c5568f7c9fdd0
Harikeshav R <r.harikeshav@gmail.com>
2024-12-25 15:10:33 -0500

    Remove unnecessary fields from the upload command response model

--- Spartan.Models/ResponseModels/Filesystem/UploadCommandResponseModel.cs
+++ Spartan.Models/ResponseModels/Filesystem/UploadCommandResponseModel.cs
@@ -5,8 +5,6 @@ namespace Spartan.Models.ResponseModels.Filesystem;
 [MessagePackObject]
 public class UploadCommandResponseModel : ICommandResponseModel
 {
-    [Key("RemoteSourceDirectoryPath")] public required string RemoteSourceDirectoryPath { get; set; }
-    [Key("LocalDestinationDirectoryPath")] public required string LocalDestinationDirectoryPath { get; set; }
     [Key("Command")] public required string Command { get; set; } = "upload";
     [Key("Output")] public required string Output { get; set; }
 }
