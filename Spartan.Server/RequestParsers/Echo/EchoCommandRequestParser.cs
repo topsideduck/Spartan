0b240f5ff366c783b9281e6984d96cfab4980e1e
Harikeshav R <r.harikeshav@gmail.com>
2024-12-24 21:34:47 -0500

    Implement the echo command request parser

--- Spartan.Server/RequestParsers/Echo/EchoCommandRequestParser.cs
+++ Spartan.Server/RequestParsers/Echo/EchoCommandRequestParser.cs
@@ -1,5 +1,15 @@
+using Spartan.Models.RequestModels;
+using Spartan.Models.RequestModels.Echo;
+
 namespace Spartan.Server.RequestParsers.Echo;
 
-public class EchoCommandRequestParser
+public class EchoCommandRequestParser : IRequestParser
 {
+    public ICommandRequestModel ParseRequest(string[] arguments)
+    {
+        return new EchoCommandRequestModel
+        {
+            Message = arguments[0]
+        };
+    }
 }
